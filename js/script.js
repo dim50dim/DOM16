2c38837b3f32eb183c17fb9d82e3747e5744adb3
dim50dim <zheliezkov@gmail.com>
2021-06-07 13:19:38 +0300

    result

--- js/script.js
+++ js/script.js
@@ -6,14 +6,16 @@
   };
 
 ////////////SECOND TASK   Напишите код, который будет менять текст в инпуте при нажатии на кнопку (изначально в инпуте уже должно быть задан текст).
+   let btn2 = document.querySelector('.btn2');
+     let a2 = document.querySelector('.input-2');
+      
+           
+      btn2.onclick = function () {
+      a2.setAttribute('value','however , we have done it');
+          console.log(a2);
+      };
 
-// let btn2 = document.querySelector('.btn2');
-//   let input = document.querySelector('.input-2');
-//     let newA = input.getAttribute('value');
-//  btn2.onclick = () => {
-//     newA.setAttribute('value' , 'move');
-//  };
-
+       
  ///////THIRD TASK Напишите код, который будет выводить alert с содержимым инпута (изначально в инпуте уже должно быть задан текст).
  let btn3 = document.querySelector('.btn3');
  let a = document.querySelector('.input-3');
@@ -36,11 +38,19 @@
 
 //////////// FIFTH TASK   Вам необходимо создать два инпута с заданными значениями и кнопку, при нажатии на которую будет осуществляться обмен содержимым между двумя инпутами (значение первого инпута должно записаться во второй, а значение второго — в первый).
 let btn5 = document.querySelector('.btn5');
+  let a51 = document.querySelector('.input-51');
+  let attrub51 = a51.getAttribute('value');
+  let a52 = document.querySelector('.input-52');
+  let attrub52 = a52.getAttribute('value');
 
+  btn5.onclick = function () {
+     a51.setAttribute('value', 'never give up');
+     a52.setAttribute('value', '100500');
+  }
 ////////SIXTH TASK  Напишите код, который будет при нажатии на кнопку менять текст в ней.\
  let btn6 = document.querySelector('.btn6');
    
-  btn6.onclick = () => {
+  btn6.onclick = function () {
       btn6.innerHTML = 'DID you catch it , we"ve changed words';
   };
 
